dec11adbd5ed0034b431c4a6d5e36258cb001f26
David Larrimore <davidlarrimore@gmail.com>
2016-09-11 23:00:09 -0400

    Prepare page from other repo functioning

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export BOWERPHP_TOKEN="$1"
+
+export PATH=/usr/local/php56/bin:$PATH
+
+export PATH=$(pwd)/.php/composer:$PATH
+
+#git pull
+
+
+composer self-update
+
+composer update
+
+composer install
+
+vendor/bin/bowerphp install
+
+#FIN
---
+++ src/AppBundle/Controller/FBTabController.php
@@ -0,0 +1,51 @@
+<?php
+
+// src/AppBundle/Controller/FBTabController.php
+namespace AppBundle\Controller;
+
+// ...
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+// src/AppBundle/Controller/LuckyController.php
+
+// ...
+class FBTabController extends Controller
+{
+    /**
+     * @Route("/facebook-tab")
+     */
+    public function facebookTab()
+    {
+        $url = 'http://funrun.lrespto.org';
+        $campaign_start_date = '09/15/2016';
+        $campaign_end_date = '10/27/2016';
+        $campaign_funding_goal = '20000';
+
+        $campaignStartDateString = strtotime($campaign_start_date);
+        $campaignStartDate = date('Y-m-d', $campaignStartDateString);
+
+        $campaignEndDateString = strtotime($campaign_end_date);
+        $campaignEndDate = date('Y-m-d', $campaignEndDateString);
+
+        $todaysDate = date('Y-m-d');
+
+        $data = [
+              'todays_date' => $todaysDate,
+              'campaign_url' => $url,
+              'campaign_start_date' => $campaignStartDate,
+              'campaign_end_date' => $campaignEndDate,
+            ];
+
+        //TODO: Make sure that only data necessary is going to the particular views....I.E. File Data
+        if ($todaysDate > $campaignEndDate) {
+            $theView = 'closeout.phtml';
+        } elseif ($todaysDate > $campaignStartDate) {
+            $theView = 'track.phtml';
+        } else {
+            $theView = 'prepare.phtml';
+        }
+
+        return $this->render('facebook-tab/prepare.html.twig', array('data' => $data));
+    }
+}
