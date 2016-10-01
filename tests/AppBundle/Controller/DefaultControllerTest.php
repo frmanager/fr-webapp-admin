5ed865fec77e9a79790c12969d214b2586cfd2e3
David Larrimore <davidlarrimore@gmail.com>
2016-10-01 15:32:58 -0400

    Setup for PHPUnit based testing.

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Tests\AppBundle\Controller;
+use AppBundle\Utils\QueryHelper;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -13,6 +14,5 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
 }
