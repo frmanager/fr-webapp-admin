a89cab5e7633ecbabf64edaef5ac7d930f4679a0
David Larrimore <davidlarrimore@gmail.com>
2017-08-04 15:34:16 -0400

    Simple user profile edit form created

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -68,4 +68,39 @@ class SecurityController extends Controller
 
       return $this->redirectToRoute('homepage');
   }
+
+
+  /**
+   * Displays a form to edit an existing User Account.
+   *
+   * @Route("/profile", name="profile_edit")
+   * @Method({"GET", "POST"})
+   */
+  public function editAction(Request $request)
+  {
+      $entity = 'User';
+      $user = $this->get('security.token_storage')->getToken()->getUser();
+      $em = $this->getDoctrine()->getManager();
+
+      //TODO: GET Referrer campaign to redirect after completion....
+
+      $editForm = $this->createForm('AppBundle\Form\UserType', $user);
+      $editForm->handleRequest($request);
+
+      if ($editForm->isSubmitted() && $editForm->isValid()) {
+          $em = $this->getDoctrine()->getManager();
+          $em->persist($grade);
+          $em->flush();
+
+          return $this->redirectToRoute('homepage');
+      }
+
+      return $this->render('security/profile.html.twig', array(
+          'user' => $user,
+          'edit_form' => $editForm->createView(),
+          'entity' => $entity
+      ));
+  }
+
+
 }
