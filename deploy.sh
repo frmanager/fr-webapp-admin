d6ae7c9608cf3e3300bce87753138a8163a7a9f6
David Larrimore <davidlarrimore@gmail.com>
2017-09-04 20:22:06 -0400

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-export BOWERPHP_TOKEN="$1"
-
-export PATH=/usr/local/php56/bin:$PATH
-
+export PATH=/usr/local/php71/bin:$PATH
 export PATH=/home/$USER/.php/composer:$PATH
-
 export SYMFONY_ENV=prod
 
 # Just in Case
@@ -13,14 +9,11 @@ git stash
 
 git pull
 
-composer self-update
-
 composer install --no-dev --optimize-autoloader
 
 php bin/console cache:clear --env=prod --no-debug
 
 php bin/console doctrine:schema:update --force
 
-vendor/bin/bowerphp install
 
 #FIN
